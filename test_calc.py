76121488fddb25eea1e6f8b9f7227b767321a4a6
MNOGOIGRAAL <artur-ismailov-01@mail.ru>
2026-02-04 16:20:03 +0500

    Добвлена функция 'subtraction' и тест для нее

--- test_calc.py
+++ test_calc.py
@@ -1,6 +1,11 @@
 from calc import addition
+from calc import subtraction
 
 
 def test_addition():
     result = addition(5, 5)
     assert result == 10
+
+def test_subtraction():
+    result = subtraction(7, 2)
+    assert result == 5
